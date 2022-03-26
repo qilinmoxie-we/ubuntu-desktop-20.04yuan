23614813c9ed733b29fadf27bedf18331c48a0ba
qilinmoxie-we <54532431+qilinmoxie-we@users.noreply.github.com>
2022-03-26 21:04:45 +0800

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,13 +1,17 @@
 sudo cp /etc/apt/sources.list  /etc/apt/sources.list.bak &&
 
-sudo echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse  \n\
-deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse  \n\
-deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse \n \
-deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse\n  \
- \n
+sudo echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse 
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
 deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse\n  \  
 deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse \n \
 deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse \n  \
 deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse\n  \
 deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse \n  \
-deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse \n " \ >> /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse  "  > /etc/apt/sources.list&&
+sudo apt update
+
+
+
+
