383b1194bd2efda1ca0354d54173503e9bb395a7
qilinmoxie-we <54532431+qilinmoxie-we@users.noreply.github.com>
2022-03-26 20:53:56 +0800

    Create search.sh

---
+++ search.sh
@@ -0,0 +1,15 @@
+sudo cp /etc/apt/sources.list  /etc/apt/sources.list &&
+Attach files by dragging & dropping, selecting or pasting them.
+
+
+sudo echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse  \n\
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse  \n\
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse \n \
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse\n  \
+ \n
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse\n  \  
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse \n \
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse \n  \
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse\n  \
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse \n  \
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse \n " \ >> /etc/apt/sources.list
